b333bbb653eac077456fcf6f06679d9040b96fae
Darshanparmarr <darshuparmar67@gmail.com>
2024-06-15 20:26:20 +0530

    Myntra Project Done

--- Projects/Myntra Functional clone/scripts/bag.js
+++ Projects/Myntra Functional clone/scripts/bag.js
@@ -1,9 +1,50 @@
+const CONVENIENCE_FEES = 99; 
 let bagItemObjects
 onLoad();
 
 function onLoad(){
   loadBagItemObjects();
   displayBagItems();
+  displayBagSummary();
+}
+
+function displayBagSummary(){
+  let bagSummaryElement = document.querySelector('.bag-summary');
+  let totalItems = bagItemObjects.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+  
+
+  bagItemObjects.forEach(bagItem => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  })
+
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+
+  bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+      <div class="price-header">PRICE DETAILS (${totalItems} Items) </div>
+      <div class="price-item">
+        <span class="price-item-tag">Total MRP</span>
+        <span class="price-item-value"> ₹ ${totalMRP}</span>
+      </div>
+      <div class="price-item">
+        <span class="price-item-tag">Discount on MRP</span>
+        <span class="price-item-value priceDetail-base-discount">- ₹ ${totalDiscount}</span>
+      </div>
+      <div class="price-item">
+        <span class="price-item-tag">Convenience Fee</span>
+        <span class="price-item-value"> ₹ 99</span>
+      </div>
+      <hr>
+      <div class="price-footer">
+        <span class="price-item-tag">Total Amount</span>
+        <span class="price-item-value"> ₹ ${finalPayment}</span>
+    </div>
+    <button class="btn-place-order">
+      <div class="css-xjhrni">PLACE ORDER</div>
+    </button>`;
 }
 
 function loadBagItemObjects(){
@@ -31,7 +72,9 @@ function removeFromBag(itemId){
   bagItems = bagItems.filter(bagItemId => bagItemId != itemId);
   localStorage.setItem('bagItems', JSON.stringify(bagItems));
   loadBagItemObjects();
+  displayBagIcon();
   displayBagItems();
+  displayBagSummary();
 }
 
 function generateItemHTML(item){
